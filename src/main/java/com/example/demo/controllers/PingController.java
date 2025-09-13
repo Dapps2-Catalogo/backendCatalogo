8667fd828f12134f92515f0c28bf1e872efb7ec4
LucianoGonzalezR <xuperlu@live.com>
2025-09-13 12:33:15 -0300

    cross origin requests from any domain

--- src/main/java/com/example/demo/controllers/PingController.java
+++ src/main/java/com/example/demo/controllers/PingController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ping")
+@CrossOrigin(origins = "*")
 public class PingController {
 
 
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -14,6 +14,7 @@ import com.example.demo.models.User;
 import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {
     
     @Autowired
